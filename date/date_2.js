23657e32fe5f1c6d39c4f4c9edea6d201ac7eec8
JJongBin <gghkdu2@gmail.com>
2021-12-13 15:46:39 +0900

    docs: fixed

--- date/date_2.js
+++ date/date_2.js
@@ -1,14 +1,5 @@
-const getDay = date => {
-  const day = new Date(date).getDay();
-
-  if (day === 0) return '일요일';
-  else if (day === 1) return '월요일';
-  else if (day === 2) return '화요일';
-  else if (day === 3) return '수요일';
-  else if (day === 4) return '목요일';
-  else if (day === 5) return '금요일';
-  else if (day === 6) return '토요일';
-};
+const day = ['일', '월', '화', '수', '목', '금', '토'];
+const getDay = date => day[new Date(date).getDay()] + '요일';
 
 console.log(getDay('2021-07-24')); // => '토요일'
 console.log(getDay('2021-07-25')); // => '일요일'
